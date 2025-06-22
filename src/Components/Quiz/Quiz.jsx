3954cad5819cb55e0a0fdc9c8275e5d3c4224074
leonardojulius <leonardojulius.jcl@gmail.com>
2025-06-22 13:47:07 +0000

    cleaning data and html structure

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () => {
+  return (
+    <div className='container'>
+
+        <h1>Quiz App</h1>
+
+        <hr />
+        <h2>Which device is required for the internnet connection?</h2>
+
+        <ul>
+            <li>Modem</li>
+            <li>Router</li>
+            <li>LAN Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+
+        <button>Next</button>
+
+        <div className="index">1 of 5</div>
+
+
+    </div>
+  )
+}
+
+export default Quiz
